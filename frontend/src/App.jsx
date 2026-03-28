54485161ba24ea2684321aa8627993a1a7ca7304
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-03-28 10:45:38 +0530

    Header and Background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
